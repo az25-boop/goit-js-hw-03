f28846e31c634704599b13e62a710d78f350b722
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-05-27 14:11:53 +0300

    date

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,8 @@
 function slugify(title) {
-    return title.toLowerCase().split(" ").join("-");
-  }
-  
-  console.log(slugify("Arrays for begginers")); // Output: "arrays-for-begginers"
-  console.log(slugify("English for developer")); // Output: "english-for-developer"
-  console.log(slugify("Ten secrets of JavaScript")); // Output: "ten-secrets-of-javascript"
-  console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // Output: "how-to-become-a-junior-developer-in-two-weeks"
-  
+  return title.toLowerCase().split(" ").join("-");
+}
+
+console.log(slugify("Arrays for begginers")); // Output: "arrays-for-begginers"
+console.log(slugify("English for developer")); // Output: "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // Output: "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // Output: "how-to-become-a-junior-developer-in-two-weeks"
