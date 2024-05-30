43356c1294a0800fde78cb4b423a23836f49194a
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-05-30 16:26:43 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -8,8 +8,8 @@ function filterArray(numbers, value) {
   return filteredArray;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // Output: [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // Output: [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // Output: []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // Output: [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // Output: [24, 41, 76]
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
